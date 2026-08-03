ab8bc762174f995ad6f6174520897b5a35b04db8
Gerald Shimo <shimogerald450@gmail.com>
2026-08-02 21:17:44 -0500

    Test: test github api

---
+++ Backend/app/clients/pittcsc.py
@@ -0,0 +1,39 @@
+import base64
+import os
+
+import requests
+from dotenv import load_dotenv
+from rich import print
+
+load_dotenv()
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+REPO_OWNER = "pittcsc"
+REPO_NAME = "Summer2026-Internships"
+
+
+def fetch_readme():
+
+    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/readme"
+
+    # headers = {
+    #     "Authorization": f"Bearer {GITHUB_TOKEN}",
+    #     "Accept": "application/vnd.github+json",
+    # }
+
+    response = requests.get(url)
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    content = base64.b64decode(data["content"]).decode("utf-8")
+
+    return content
+
+
+if __name__ == "__main__":
+    readme = fetch_readme()
+    print("=" * 100)
+    print(readme[:10000])
