4346d48d097f5175d918f2d73725ed03ea90412e
Gerald Shimo <shimogerald450@gmail.com>
2026-08-04 00:01:50 -0500

    chore: cleanup code for github soucery

--- Backend/app/processors/pittParser.py
+++ Backend/app/processors/pittParser.py
@@ -39,7 +39,7 @@ def parse_jobs(html):
                     "location": location,
                     "apply_url": link,
                     "age": age,
-                    "source": "pittscs",
+                    "source": "pittcsc",
                 }
             )
 
